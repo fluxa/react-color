054b17e358de14e87c607c941df24225eaecaa44
Fluxà <jmfluxa@gmail.com>
2019-08-13 23:13:55 -0400

    shrinking

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-export { default as AlphaPicker } from './components/alpha/Alpha'
-export { default as BlockPicker } from './components/block/Block'
-export { default as CompactPicker } from './components/compact/Compact'
-export { default as HuePicker } from './components/hue/Hue'
-export { default as SliderPicker } from './components/slider/Slider'
-export { default as SwatchesPicker } from './components/swatches/Swatches'
 
+export { default as SwatchesPicker } from './components/swatches/Swatches'
 export { default as CustomPicker } from './components/common/ColorWrap'
